7f5bf30614c03608f78d4e313260f04e621c82ad
Sam Meech-Ward <sam@meech-ward.com>
2019-08-21 11:55:48 -0700

    😎

---
+++ precache-manifest.60968dab2dcd2ddf73b069ab6ea56e67.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "887ecbcf3cb0ffad356dfb90386ce0be",
+    "url": "/index.html"
+  },
+  {
+    "revision": "0bee3bb1d9c2b69918b4",
+    "url": "/static/css/main.feccbfaa.chunk.css"
+  },
+  {
+    "revision": "5e563e928b94e887ae29",
+    "url": "/static/js/2.d6007410.chunk.js"
+  },
+  {
+    "revision": "0bee3bb1d9c2b69918b4",
+    "url": "/static/js/main.01a0ce99.chunk.js"
+  },
+  {
+    "revision": "c30fbcdeba4d4fd41528",
+    "url": "/static/js/runtime~main.c2db9949.js"
+  }
+]);
