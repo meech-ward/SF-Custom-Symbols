7dd0467fd15ba34ee2d06da459fbf340a58cf98e
Sam Meech-Ward <sam@meech-ward.com>
2019-08-21 12:04:14 -0700

    😎

---
+++ precache-manifest.28b41ebf948fd451783f8ed1fac7bfe9.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "277aeead643d7d204bda01ff72b716f8",
+    "url": "/SF-Custom-Symbols/index.html"
+  },
+  {
+    "revision": "9445fafde6bb2b74fbb5",
+    "url": "/SF-Custom-Symbols/static/css/main.feccbfaa.chunk.css"
+  },
+  {
+    "revision": "5e563e928b94e887ae29",
+    "url": "/SF-Custom-Symbols/static/js/2.d6007410.chunk.js"
+  },
+  {
+    "revision": "9445fafde6bb2b74fbb5",
+    "url": "/SF-Custom-Symbols/static/js/main.2cc11d07.chunk.js"
+  },
+  {
+    "revision": "3d1a082c49db0ff67efb",
+    "url": "/SF-Custom-Symbols/static/js/runtime~main.66ff7c33.js"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.60968dab2dcd2ddf73b069ab6ea56e67.js"
+  "/SF-Custom-Symbols/precache-manifest.28b41ebf948fd451783f8ed1fac7bfe9.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/SF-Custom-Symbols/index.html"), {
   
   blacklist: [/^\/_/,/\/[^\/?]+\.[^\/]+$/],
 });
