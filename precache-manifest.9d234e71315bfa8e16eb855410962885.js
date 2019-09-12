692c73f7f49180fbc3cda835c5e0f8e4fb3a7603
Sam Meech-Ward <sam@meech-ward.com>
2019-09-11 20:55:48 -0700

    🤗'

---
+++ precache-manifest.9d234e71315bfa8e16eb855410962885.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e25689861e36a3291a256c0cb32cc6da",
+    "url": "/index.html"
+  },
+  {
+    "revision": "91251c18800d92656501",
+    "url": "/static/css/main.8d55bd3b.chunk.css"
+  },
+  {
+    "revision": "e7b953e63a07f22fe50a",
+    "url": "/static/js/2.053d1125.chunk.js"
+  },
+  {
+    "revision": "91251c18800d92656501",
+    "url": "/static/js/main.a3445a79.chunk.js"
+  },
+  {
+    "revision": "c30fbcdeba4d4fd41528",
+    "url": "/static/js/runtime~main.c2db9949.js"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.60968dab2dcd2ddf73b069ab6ea56e67.js"
+  "/precache-manifest.9d234e71315bfa8e16eb855410962885.js"
 );
 
 self.addEventListener('message', (event) => {
